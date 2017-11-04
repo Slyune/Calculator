b3bdf324183e0bbabbee710ed18403cccc559445
Slyune <406618060@qq.com>
2017-11-04 09:39:37 +0800

    Slyune

--- app/src/main/java/com/slyune/calculator/MainActivity.java
+++ app/src/main/java/com/slyune/calculator/MainActivity.java
@@ -2,12 +2,43 @@ package com.slyune.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText input;
+    private TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inist();
     }
+
+    private void inist() {
+        findViewById(R.id.btnNum0).setOnClickListener(this);
+        findViewById(R.id.btnNum1).setOnClickListener(this);
+        findViewById(R.id.btnNum2).setOnClickListener(this);
+        findViewById(R.id.btnNum3).setOnClickListener(this);
+        findViewById(R.id.btnNum4).setOnClickListener(this);
+        findViewById(R.id.btnNum5).setOnClickListener(this);
+        findViewById(R.id.btnNum6).setOnClickListener(this);
+        findViewById(R.id.btnNum7).setOnClickListener(this);
+        findViewById(R.id.btnNum8).setOnClickListener(this);
+        findViewById(R.id.btnNum9).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnNum0:
+
+                break;
+        }
+    }
+
 }
